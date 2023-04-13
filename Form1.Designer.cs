6c0de1fd2554431c8b6ca8e82346ac7291283694
FengXin <1638547819@qq.com>
2023-04-13 15:05:10 +0800

    加入下拉框

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,17 +33,16 @@
             this.MessageBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // LogoLabel
             // 
             this.LogoLabel.AutoSize = true;
             this.LogoLabel.Font = new System.Drawing.Font("Segoe Print", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.LogoLabel.Location = new System.Drawing.Point(216, 8);
-            this.LogoLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LogoLabel.Location = new System.Drawing.Point(278, 9);
             this.LogoLabel.Name = "LogoLabel";
-            this.LogoLabel.Size = new System.Drawing.Size(272, 61);
+            this.LogoLabel.Size = new System.Drawing.Size(332, 75);
             this.LogoLabel.TabIndex = 0;
             this.LogoLabel.Text = "SNL Compiler";
             // 
@@ -53,12 +52,12 @@
             this.CodeBox.BackColor = System.Drawing.SystemColors.Control;
             this.CodeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.CodeBox.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.CodeBox.Location = new System.Drawing.Point(0, 76);
-            this.CodeBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.CodeBox.Location = new System.Drawing.Point(0, 89);
+            this.CodeBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.CodeBox.Multiline = true;
             this.CodeBox.Name = "CodeBox";
             this.CodeBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.CodeBox.Size = new System.Drawing.Size(751, 647);
+            this.CodeBox.Size = new System.Drawing.Size(965, 761);
             this.CodeBox.TabIndex = 1;
             this.CodeBox.TabStop = false;
             this.CodeBox.WordWrap = false;
@@ -69,23 +68,24 @@
             this.MessageBox.AcceptsTab = true;
             this.MessageBox.BackColor = System.Drawing.SystemColors.Control;
             this.MessageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.MessageBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.MessageBox.Location = new System.Drawing.Point(751, 76);
-            this.MessageBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.MessageBox.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MessageBox.ForeColor = System.Drawing.Color.Firebrick;
+            this.MessageBox.Location = new System.Drawing.Point(966, 89);
+            this.MessageBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MessageBox.Multiline = true;
             this.MessageBox.Name = "MessageBox";
             this.MessageBox.ReadOnly = true;
             this.MessageBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.MessageBox.Size = new System.Drawing.Size(315, 647);
+            this.MessageBox.Size = new System.Drawing.Size(404, 761);
             this.MessageBox.TabIndex = 2;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(751, 10);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Location = new System.Drawing.Point(988, 12);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(131, 55);
+            this.button1.Size = new System.Drawing.Size(168, 65);
             this.button1.TabIndex = 3;
             this.button1.Text = "Analysis";
             this.button1.UseVisualStyleBackColor = true;
@@ -94,38 +94,43 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(917, 8);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Location = new System.Drawing.Point(1180, 9);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(131, 55);
+            this.button2.Size = new System.Drawing.Size(168, 65);
             this.button2.TabIndex = 4;
             this.button2.Text = "Format";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
-            // comboBox1
+            // comboBox
             // 
-            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.ItemHeight = 49;
-            this.comboBox1.Location = new System.Drawing.Point(590, 10);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 55);
-            this.comboBox1.TabIndex = 5;
+            this.comboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.ItemHeight = 59;
+            this.comboBox.Location = new System.Drawing.Point(764, 12);
+            this.comboBox.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox.MaxDropDownItems = 5;
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(195, 65);
+            this.comboBox.TabIndex = 5;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1076, 732);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1383, 861);
+            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.MessageBox);
             this.Controls.Add(this.CodeBox);
             this.Controls.Add(this.LogoLabel);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.MinimumSize = new System.Drawing.Size(1092, 771);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(1399, 899);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -141,6 +146,6 @@
         private TextBox MessageBox;
         private Button button1;
         private Button button2;
-        private ComboBox comboBox1;
+        private ComboBox comboBox;
     }
 }
